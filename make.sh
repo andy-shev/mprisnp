8a858874bc408acfc6c1a2a453e3d0b6e2eb34ff
Andy Shevchenko <andy.shevchenko@gmail.com>
2010-10-15 23:29:46 +0300

    Move main_dbus.c functionality to the lj_dbus.c
    Signed-off-by: Andy Shevchenko <andy.shevchenko@gmail.com>

--- make.sh
+++ make.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-gcc -O2 -Wall -W -Wextra -g -ggdb -o lj_dbus main_dbus.c `pkg-config --cflags --libs dbus-1 dbus-glib-1`
+
+gcc -O2 -Wall -W -Wextra -g -ggdb -o lj_dbus lj_dbus.c		\
+	-DLJ_DBUS_CONSOLE_TEST					\
+	$(pkg-config --cflags --libs dbus-1 dbus-glib-1)
